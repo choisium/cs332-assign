6016d532f5204ddf0f160fad47b5a1081e792618
Soomin Choi <choisium@gmail.com>
2022-09-09 20:27:00 +0900

    assn1: implement exercise functions

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,32 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance_aux(chars: List[Char], count_left: Int): Boolean =
+      if (chars.isEmpty) count_left == 0
+      else if (count_left < 0) false
+      else if (chars.head == '(') balance_aux(chars.tail, count_left + 1)
+      else if (chars.head == ')') balance_aux(chars.tail, count_left - 1)
+      else balance_aux(chars.tail, count_left)
+    balance_aux(chars, 0)
+  }
+
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (coins.isEmpty) 0
+    else if (coins.head > money) countChange(money, coins.tail)
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
