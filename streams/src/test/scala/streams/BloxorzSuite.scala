ad495372d9a74514a9c5ae3374bb210570eafc58
Soomin Choi <choisium@gmail.com>
2022-09-12 00:19:16 +0900

    assn6: implement Solver and add test cases

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -53,6 +53,34 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("neighborsWithHistory level 1") {
+    new Level1 {
+      val b = Block(Pos(1,1), Pos(1,1))
+      val h = List(Left, Up)
+      val n = Set(
+        (Block(Pos(1,2), Pos(1,3)), List(Right, Left, Up)),
+        (Block(Pos(2,1), Pos(3,1)), List(Down, Left, Up))
+      ).toStream
+
+      assert(neighborsWithHistory(b, h) == n)
+    }
+  }
+
+  test("newNeighborsOnly level 1") {
+    new Level1 {
+      val e = Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      val n1 = Set(
+        (Block(Pos(1,2), Pos(1,3)), List(Right, Left, Up)),
+        (Block(Pos(2,1), Pos(3,1)), List(Down, Left, Up))
+      ).toStream
+      val n2 = Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream
+
+      assert(newNeighborsOnly(n1, e) == n2)
+    }
+  }
+
   test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
